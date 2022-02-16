8b79cf33a0a9ce9453202bf36187ab13cae284cb
Tom Keeley <tom.keeley@arup.com>
2022-02-16 10:16:29 +0000

    Implemented UserPreview component

---
+++ components/user-preview/UserPreview.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box, Text, Button, HStack, Flex, Center } from 'native-base'
+import propTypes from 'prop-types'
+
+export default function UserPreview ({ id, firstName, lastName }) {
+  return (
+    <Box bg={'white'} pt={'2'} m={'2'} borderRadius={'5'} shadow={'5'}>
+      <HStack>
+        <Box w={'100%'}>
+          <Box>
+            <Text alignSelf={'center'}>{firstName} {lastName}</Text>
+          </Box>
+          <Box alignItems={'right'}>
+            <Center>
+              <HStack>
+                <Flex direction='row'>
+                  <Button variant={'ghost'}>Add Friend</Button>
+                  <Button variant={'ghost'}>See Friends</Button>
+                </Flex>
+              </HStack>
+            </Center>
+          </Box>
+        </Box>
+      </HStack>
+    </Box>
+  )
+}
+
+UserPreview.propTypes = {
+  id: propTypes.string.isRequired,
+  firstName: propTypes.string.isRequired,
+  lastName: propTypes.string.isRequired
+}
--- screens/friends-screen/FriendsScreen.jsx
+++ screens/friends-screen/FriendsScreen.jsx
@@ -1,26 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { VStack, HStack, Center, Input, Icon, Box, Radio, Stack, Select, CheckIcon } from 'native-base'
+import React, { useContext, useEffect, useState } from 'react'
+import { VStack, HStack, Center, Input, Icon, Box, Radio, ScrollView } from 'native-base'
 import { MaterialIcons } from '@expo/vector-icons'
+
+// Context API
+import { SpaceBookContext } from '../../context/SpacebookContext'
 
 // Custom imports
 import SearchOptions from '../../components/search-options/SearchOptions'
+import UserPreview from '../../components/user-preview/UserPreview'
+
+// https://editor.swagger.io/search?q=Ash%20Williams&limit=20&offset=0
 
 export default function FriendsScreen () {
   const [checkBoxValue, setCheckBoxValue] = useState('friends')
+  const { pagination, token } = useContext(SpaceBookContext)
+  const [users, setUsers] = useState([])
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`http://localhost:3333/api/1.0.0/search?limit=${pagination}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'X-Authorization': token
+        }
+      })
+      setUsers(await response.json())
+    } catch (err) {
+      console.log(err)
+    }
+  }, [])
 
   useEffect(() => {
-    console.log(checkBoxValue)
-  }, [checkBoxValue])
+    console.log(users)
+  }, [users])
 
   return (
     <Box safeArea w={'100%'} h={'100%'}>
       <VStack p='5'>
-      <Box bg={'white'} py={'2'} px={'3'} m={'1'} borderRadius={'5'} shadow={'5'}>
-        <HStack>
-          <Input placeholder="Search People & Places" w={'90%'} borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />}/>
-          <SearchOptions />
-        </HStack>
-      </Box>
+        <Box bg={'white'} py={'2'} px={'3'} m={'1'} borderRadius={'5'} shadow={'5'}>
+          <HStack>
+            <Input placeholder="Search People & Places" w={'90%'} borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />}/>
+            <SearchOptions />
+          </HStack>
+        </Box>
         <Radio.Group value={checkBoxValue} onChange={nextValue => { setCheckBoxValue(nextValue) }} name='friendsGroup' accessibilityLabel='Selct your friends or search for a friend'>
           <Center w={'100%'}>
             <HStack p={'2'}>
@@ -37,6 +61,13 @@ export default function FriendsScreen () {
             </HStack>
           </Center>
         </Radio.Group>
+        <ScrollView h={'650'}>
+          {users.map(user => {
+            return (
+              <UserPreview key={user.user_id} id={user.user_id} firstName={user.user_givenname} lastName={user.user_familyname} />
+            )
+          })}
+        </ScrollView>
       </VStack>
     </Box>
   )
