82c9bef67057a568d426a2c2463553211f9d195f
Tom Keeley <tom.keeley@arup.com>
2022-02-23 09:50:21 +0000

    Removed unneed code and feed screen

--- screens/home-screen/HomeScreen.jsx
+++ screens/home-screen/HomeScreen.jsx
@@ -5,16 +5,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 // Custom imports
 import ProfileScreen from '../profile-screen/ProfileScreen'
 import FriendsScreen from '../friends-screen/FriendsScreen'
-import FeedScreen from '../feed-screen/FeedScreen'
 import SettingsScreen from '../settings-screen/SettingsScreen'
-import { Ionicons, FontAwesome, FontAwesome5 } from '@expo/vector-icons'
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons'
 
 export default function HomeScreen () {
   const Tab = createBottomTabNavigator()
   return (
     <NativeBaseProvider theme={theme}>
       <Tab.Navigator initialRouteName='Profile' screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Feed" component={FeedScreen} options={{ tabBarIcon: () => (<FontAwesome name='feed' size={24} color='black' />) }}/>
         <Tab.Screen name="Profile" component={ProfileScreen} options={{ tabBarIcon: () => (<Ionicons name='person' size={24} color='black' />) }}/>
         <Tab.Screen name="Friends" component={FriendsScreen} options={{ tabBarIcon: () => (<FontAwesome5 name='user-friends' size={24} color='black' />) }}/>
         <Tab.Screen name="Settings" component={SettingsScreen} options={{ tabBarIcon: () => (<Ionicons name='settings' size={24} color='black' />) }}/>
